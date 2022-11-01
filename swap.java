e57ff045dea5cc157a1c0b394250bb64f40c56cd
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-11-01 11:17:41 +0530

    swap.java

---
+++ swap.java
@@ -0,0 +1,16 @@
+public class swap {
+    public static void swaping(){
+        float first = 1.20f;
+        float second = 3.30f;
+        System.out.println("=====befor swab=====");
+        System.out.println("first number =" + " " + first);
+        System.out.println("second number = " + " " + second);
+
+        float temp = first;
+//        second = temp;
+
+        System.out.println("====after swap=====");
+        System.out.println("first number =" + " " + second);
+        System.out.println("second number = " + " " +first );
+    }
+}
